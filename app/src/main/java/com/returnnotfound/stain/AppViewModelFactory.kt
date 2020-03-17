856d556853c6283d14fe7112544a014b4c5c9846
giangdinh <giangdinh96@gmail.com>
2020-03-17 15:26:38 +0700

    Commit

---
+++ app/src/main/java/com/returnnotfound/stain/AppViewModelFactory.kt
@@ -0,0 +1,23 @@
+package com.returnnotfound.stain
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+class AppViewModelFactory @Inject internal constructor(private val creators: MutableMap<Class<out ViewModel>, Provider<ViewModel>>) :
+  ViewModelProvider.Factory {
+  @Suppress("UNCHECKED_CAST")
+  override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    val creator: Provider<out ViewModel>? = creators[modelClass]
+    requireNotNull(creator) { "Unknown model class $modelClass" }
+    return try {
+      creator.get() as T
+    } catch (e: Exception) {
+      throw RuntimeException(e)
+    }
+  }
+
+}
