896a56ee96bc9daeff2c2bdef46df037d508581e
giangdinh <giangdinh96@gmail.com>
2020-03-19 19:11:36 +0700

    Commit

---
+++ app/src/main/java/com/returnnotfound/stain/di/component/AppComponent.kt
@@ -0,0 +1,26 @@
+package com.returnnotfound.stain.di.component
+
+import com.returnnotfound.stain.App
+import com.returnnotfound.stain.di.module.*
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+  modules = [
+    AndroidSupportInjectionModule::class,
+    AppModule::class,
+    FontModule::class,
+    NetworkConfigModule::class,
+    ActivityBuilder::class,
+    ViewModelModule::class,
+    ApiServiceModule::class,
+    RepositoryModule::class
+  ]
+)
+interface AppComponent : AndroidInjector<App> {
+  @Component.Factory
+  interface Factory : AndroidInjector.Factory<App>
+}
