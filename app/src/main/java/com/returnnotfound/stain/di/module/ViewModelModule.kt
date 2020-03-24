e391ee49834079ae82abbe4a0bd093b09543324e
giangdinh <giangdinh96@gmail.com>
2020-03-24 23:14:33 +0700

    update

--- app/src/main/java/com/returnnotfound/stain/di/module/ViewModelModule.kt
+++ app/src/main/java/com/returnnotfound/stain/di/module/ViewModelModule.kt
@@ -2,6 +2,7 @@ package com.returnnotfound.stain.di.module
 
 import androidx.lifecycle.ViewModel
 import com.returnnotfound.stain.di.ViewModelKey
+import com.returnnotfound.stain.presentation.main.discover.DiscoverViewModel
 import com.returnnotfound.stain.presentation.main.rank.RankViewModel
 import dagger.Binds
 import dagger.Module
@@ -10,8 +11,13 @@ import dagger.multibindings.IntoMap
 @Module
 abstract class ViewModelModule {
 
+  @ViewModelKey(DiscoverViewModel::class)
+  @IntoMap
+  @Binds
+  abstract fun bindDiscoverViewModel(discoverViewModel: DiscoverViewModel): ViewModel
+
   @ViewModelKey(RankViewModel::class)
   @IntoMap
   @Binds
-  abstract fun bindRankVM(rankVM: RankViewModel): ViewModel
+  abstract fun bindRankViewModel(rankViewModel: RankViewModel): ViewModel
 }
