dd377bc4a7925f9d977035032954ddfee56702cb
giangdinh <giangdinh96@gmail.com>
2020-03-25 12:05:53 +0700

    move

--- app/src/main/java/com/returnnotfound/stain/di/module/DataSourceModule.kt
+++ app/src/main/java/com/returnnotfound/stain/di/module/DataSourceModule.kt
@@ -1,9 +1,9 @@
 package com.returnnotfound.stain.di.module
 
-import com.returnnotfound.stain.data.source.BannerLocalSource
-import com.returnnotfound.stain.data.source.BannerLocalSourceImpl
-import com.returnnotfound.stain.data.source.BannerRemoteSource
-import com.returnnotfound.stain.data.source.BannerRemoteSourceImpl
+import com.returnnotfound.stain.data.repository.source.BannerLocalSource
+import com.returnnotfound.stain.data.repository.source.BannerLocalSourceImpl
+import com.returnnotfound.stain.data.repository.source.BannerRemoteSource
+import com.returnnotfound.stain.data.repository.source.BannerRemoteSourceImpl
 import dagger.Binds
 import dagger.Module
 
