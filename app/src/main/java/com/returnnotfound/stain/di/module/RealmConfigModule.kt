a98c3e44c20dd3e504cf6a67671be9e9c35c5365
giangdinh <giangdinh96@gmail.com>
2020-03-28 10:50:01 +0700

    add realm config

---
+++ app/src/main/java/com/returnnotfound/stain/di/module/RealmConfigModule.kt
@@ -0,0 +1,43 @@
+package com.returnnotfound.stain.di.module
+
+import dagger.Module
+import dagger.Provides
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import javax.inject.Singleton
+
+@Module
+abstract class RealmConfigModule {
+
+  companion object {
+    @JvmStatic
+    @Singleton
+    @Provides
+    fun provideRealmConfig(): RealmConfiguration =
+      RealmConfiguration
+        .Builder()
+        .name("MyRealm.realm")
+        .schemaVersion(1)
+        .initialData {
+          // Init data
+        }
+        .migration { realm, old, new ->
+          var oldVersion = old
+          if (oldVersion == 1L) {
+
+            oldVersion++
+          }
+
+          if (oldVersion == 2L) {
+
+            oldVersion++
+          }
+        }
+        .build()
+
+    @JvmStatic
+    @Provides
+    fun provideRealm(realmConfiguration: RealmConfiguration): Realm =
+      Realm.getInstance(realmConfiguration)
+  }
+}
