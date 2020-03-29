4f8f21d386d1f7a68ad557829054fcc2c0c06a81
giangdinh <giangdinh96@gmail.com>
2020-03-29 08:47:24 +0700

    add image

--- app/src/main/java/com/returnnotfound/stain/di/module/FontModule.kt
+++ app/src/main/java/com/returnnotfound/stain/di/module/FontModule.kt
@@ -11,33 +11,31 @@ import io.github.inflationx.viewpump.ViewPump
 import javax.inject.Singleton
 
 @Module
-class FontModule {
-  companion object {
-    @JvmStatic
-    @FontPathDefault
-    @Singleton
-    @Provides
-    fun provideFontPathDefault(): String {
-      return FONT_DEFAULT_PATH
-    }
+object FontModule {
+  @JvmStatic
+  @FontPathDefault
+  @Singleton
+  @Provides
+  fun provideFontPathDefault(): String {
+    return FONT_DEFAULT_PATH
+  }
 
-    @JvmStatic
-    @Singleton
-    @Provides
-    fun provideCalligraphyInterceptor(@FontPathDefault fontPathDefault: String): CalligraphyInterceptor {
-      return CalligraphyInterceptor(
-        CalligraphyConfig.Builder()
-          .setDefaultFontPath(fontPathDefault)
-          .setFontAttrId(R.attr.fontPath)
-          .build()
-      )
-    }
+  @JvmStatic
+  @Singleton
+  @Provides
+  fun provideCalligraphyInterceptor(@FontPathDefault fontPathDefault: String): CalligraphyInterceptor {
+    return CalligraphyInterceptor(
+      CalligraphyConfig.Builder()
+        .setDefaultFontPath(fontPathDefault)
+        .setFontAttrId(R.attr.fontPath)
+        .build()
+    )
+  }
 
-    @JvmStatic
-    @Singleton
-    @Provides
-    fun provideViewPump(calligraphyInterceptor: CalligraphyInterceptor): ViewPump {
-      return ViewPump.builder().addInterceptor(calligraphyInterceptor).build()
-    }
+  @JvmStatic
+  @Singleton
+  @Provides
+  fun provideViewPump(calligraphyInterceptor: CalligraphyInterceptor): ViewPump {
+    return ViewPump.builder().addInterceptor(calligraphyInterceptor).build()
   }
 }
--- app/src/main/java/com/returnnotfound/stain/di/module/RealmConfigModule.kt
+++ app/src/main/java/com/returnnotfound/stain/di/module/RealmConfigModule.kt
@@ -7,37 +7,34 @@ import io.realm.RealmConfiguration
 import javax.inject.Singleton
 
 @Module
-abstract class RealmConfigModule {
+object RealmConfigModule {
+  @JvmStatic
+  @Singleton
+  @Provides
+  fun provideRealmConfig(): RealmConfiguration =
+    RealmConfiguration
+      .Builder()
+      .name("MyRealm.realm")
+      .schemaVersion(1)
+      .initialData {
+        // Init data
+      }
+      .migration { realm, old, new ->
+        var oldVersion = old
+        if (oldVersion == 1L) {
 
-  companion object {
-    @JvmStatic
-    @Singleton
-    @Provides
-    fun provideRealmConfig(): RealmConfiguration =
-      RealmConfiguration
-        .Builder()
-        .name("MyRealm.realm")
-        .schemaVersion(1)
-        .initialData {
-          // Init data
+          oldVersion++
         }
-        .migration { realm, old, new ->
-          var oldVersion = old
-          if (oldVersion == 1L) {
 
-            oldVersion++
-          }
+        if (oldVersion == 2L) {
 
-          if (oldVersion == 2L) {
-
-            oldVersion++
-          }
+          oldVersion++
         }
-        .build()
+      }
+      .build()
 
-    @JvmStatic
-    @Provides
-    fun provideRealm(realmConfiguration: RealmConfiguration): Realm =
-      Realm.getInstance(realmConfiguration)
-  }
+  @JvmStatic
+  @Provides
+  fun provideRealm(realmConfiguration: RealmConfiguration): Realm =
+    Realm.getInstance(realmConfiguration)
 }
