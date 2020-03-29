b2826316569a608fc8ae01260aebc12b1c347784
giangdinh <giangdinh96@gmail.com>
2020-03-29 09:06:04 +0700

    add image

--- app/src/main/java/com/returnnotfound/stain/di/module/RealmConfigModule.kt
+++ app/src/main/java/com/returnnotfound/stain/di/module/RealmConfigModule.kt
@@ -4,37 +4,51 @@ import dagger.Module
 import dagger.Provides
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import io.realm.RealmMigration
 import javax.inject.Singleton
 
 @Module
 object RealmConfigModule {
   @JvmStatic
+  @Provides
+  fun provideRealm(realmConfiguration: RealmConfiguration): Realm =
+    Realm.getInstance(realmConfiguration)
+
+  @JvmStatic
   @Singleton
   @Provides
-  fun provideRealmConfig(): RealmConfiguration =
+  fun provideRealmConfig(
+    realmTransaction: Realm.Transaction,
+    migration: RealmMigration
+  ): RealmConfiguration =
     RealmConfiguration
       .Builder()
       .name("MyRealm.realm")
       .schemaVersion(1)
-      .initialData {
-        // Init data
-      }
-      .migration { realm, old, new ->
-        var oldVersion = old
-        if (oldVersion == 1L) {
-
-          oldVersion++
-        }
-
-        if (oldVersion == 2L) {
-
-          oldVersion++
-        }
-      }
+      .initialData(realmTransaction)
+      .migration(migration)
       .build()
 
   @JvmStatic
+  @Singleton
   @Provides
-  fun provideRealm(realmConfiguration: RealmConfiguration): Realm =
-    Realm.getInstance(realmConfiguration)
+  fun provideInitData(): Realm.Transaction = Realm.Transaction {
+
+  }
+
+  @JvmStatic
+  @Singleton
+  @Provides
+  fun provideMigration(): RealmMigration = RealmMigration { realm, old, new ->
+    var oldVersion = old
+    if (oldVersion == 1L) {
+
+      oldVersion++
+    }
+
+    if (oldVersion == 2L) {
+
+      oldVersion++
+    }
+  }
 }
