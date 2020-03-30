6ae2bdeb848fe4adfeb6959c8eb046c236be8b77
giangdinh <giangdinh96@gmail.com>
2020-03-30 19:23:39 +0700

    add image

--- app/src/main/java/com/returnnotfound/stain/base/binding/ImageBindingAdapter.kt
+++ app/src/main/java/com/returnnotfound/stain/base/binding/ImageBindingAdapter.kt
@@ -2,6 +2,9 @@ package com.returnnotfound.stain.base.binding
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.returnnotfound.stain.base.extension.loadImage
+import com.returnnotfound.stain.base.extension.loadImageCircle
+import com.returnnotfound.stain.base.extension.loadImageRound
 
 @BindingAdapter("android:url", "android:circle", "android:corner", requireAll = false)
 fun setImage(imageView: ImageView, url: String, circle: Boolean, corner: Float) {
