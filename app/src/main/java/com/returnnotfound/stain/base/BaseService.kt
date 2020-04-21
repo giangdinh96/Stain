89702fe663b448547c319e8b5d6a625751a9f307
giangdinh <giangdinh96@gmail.com>
2020-04-21 19:47:00 +0700

    add image

--- app/src/main/java/com/returnnotfound/stain/base/BaseService.kt
+++ app/src/main/java/com/returnnotfound/stain/base/BaseService.kt
@@ -1,7 +1,8 @@
-package com.returnnotfound.stain.presentation.ui
+package com.returnnotfound.stain.base
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.Build
@@ -11,7 +12,7 @@ import androidx.core.app.NotificationCompat
 import com.returnnotfound.stain.R
 
 
-class MyService : Service() {
+class BaseService : Service() {
   override fun onBind(intent: Intent?): IBinder? {
     return null
   }
@@ -20,10 +21,10 @@ class MyService : Service() {
     // Create the NotificationChannel, but only on API 26+ because
     // the NotificationChannel class is new and not in the support library
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      val name: CharSequence = "getString(R.string.channel_name)"
-      val description = "getString(R.string.channel_description)"
+      val name: CharSequence = "MyMusic"
+      val description = "My music app channel"
       val importance = NotificationManager.IMPORTANCE_DEFAULT
-      val channel = NotificationChannel("123", name, importance)
+      val channel = NotificationChannel("1996", name, importance)
       channel.description = description
       // Register the channel with the system; you can't change the importance
       // or other notification behaviors after this
@@ -36,21 +37,33 @@ class MyService : Service() {
     logTag("onCreate")
     super.onCreate()
     logTag("onCreate")
-    createNotificationChannel()
-    startForeground(
-      1,
-      NotificationCompat
-        .Builder(this, "123")
-        .setContentTitle("Title")
-        .setContentTitle("Content title")
-        .setContentText("Content text")
-        .setSmallIcon(R.mipmap.ic_launcher)
-        .build()
-    )
   }
 
   override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
     logTag("onStartCommand $intent $flags $startId")
+    createNotificationChannel()
+    val pendingIntent = PendingIntent.getService(
+      this,
+      1,
+      Intent(this, BaseService::class.java),
+      PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    startForeground(
+      1,
+      NotificationCompat
+        .Builder(this, "123")
+        .addAction(0, "Pre", pendingIntent)
+        .addAction(R.drawable.ic_more, null, pendingIntent)
+        .addAction(0, "Next", pendingIntent)
+        .addAction(0, "Close", pendingIntent)
+        .setSmallIcon(R.mipmap.ic_launcher)
+        .setContentText("Content text")
+        .setTicker("Ticker")
+        .setSubText("Sub text")
+        .setContentTitle("content title")
+        .build()
+    )
     return super.onStartCommand(intent, flags, startId)
   }
 
